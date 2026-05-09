d409636b1eeb026103208ccae4b3ae167bc44f9d
Famousmew <famousmewoffical3709@gmail.com>
2026-05-09 14:56:23 +0900

    Update atlases.lua

--- atlases.lua
+++ atlases.lua
@@ -3,14 +3,14 @@
 -- Make sure to make two images, one at 71x95 and 142x190 or else the game will crash
 
 SMODS.Atlas{
-  key = "kms_joker",
-  path = "j_kms.png",
-  px = 71,
-  py = 95
+key = "kms_joker", -- Iibjtkms?
+path = "j_kms.png",
+px = 71,
+py = 95
 }
 
 SMODS.Atlas{
-key = "one_one_highcard",
+key = "one_one_highcard", -- WE'RE BACK IN 1-1 AGAIN!?!?!
 path = "b_ooh.png",
 px = 34,
 py = 34
@@ -24,21 +24,21 @@ py = 95
 }
 
 SMODS.Atlas{
-key = "noir_joker",
+key = "noir_joker", -- Noir is the best
 path = "J_noir.png",
 px = 71,
 py = 95
 }
 
 SMODS.Atlas{
-key = "kumo_joker",
+key = "kumo_joker", -- Rae is also the best
 path = "j_kumo.png",
 px = 71,
 py = 95
 }
 
-SMODS.Atlas{
-key = "feh_joker",
+SMODS.Atlas{ 
+key = "feh_joker", -- The fucking game Noir is addicted to fsr
 path = "j_feh.png",
 px = 71,
 py = 95
@@ -66,7 +66,7 @@ py = 95
 }
 
 SMODS.Atlas{
-key = "egg_tarot",
+key = "egg_tarot", -- Egg.
 path = "t_egg.png",
 px = 71,
 py = 95
